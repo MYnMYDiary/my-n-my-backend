95fadf401b6663be4633f82f027501a81e92dcbe
Soyeon <isylsy166@gmail.com>
2025-01-26 05:16:57 +0900

    cors 문제

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: "http://localhost:3000" }); // Next.js URL 허용
   await app.listen(8080);
 }
 bootstrap();
